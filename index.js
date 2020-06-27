53e6d8aa3f61aee1a406d24424419f29652be30e
Joe Laurle <Josue.laurole@gmail.com>
2020-06-27 14:29:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+Import React, { Componet } from 'react';
+
+Class App extends Componet {
+	render () {
+		return (
+			<div className="App">
+				<h1>Hello World!</h1>
+			</div>
+		);
+	}
+}
+
+Export default App;
