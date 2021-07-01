f7eb58e4a909cc22d808178c731425b638920c2c
Joe Laurole <Josue.laurole@gmail.com>
2021-07-01 12:13:11 -0400

    Create UpdatingCSS.js

---
+++ UpdatingCSS.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+      <title>Scoped CSS Variable and JS</title>
+</head>
+<body>
+        <h2>Update CSS Variable with <span class='h1'>JS</span></h2>
+          
+          <div class="controls">
+            <label for="spacing>
